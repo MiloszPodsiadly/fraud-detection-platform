de278e0dad79c4e39d93dcc42263cd411f2f5a34
Milosz Podsiadly <m.podsiadly99@gmail.com>
2026-06-19 13:00:08 +0200

    refactor(cache): isolate application cache configuration

---
+++ alert-service/src/main/java/com/frauddetection/alert/config/ApplicationCacheConfiguration.java
@@ -0,0 +1,9 @@
+package com.frauddetection.alert.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+@EnableCaching
+public class ApplicationCacheConfiguration {
+}
--- audit-trust-authority/src/main/java/com/frauddetection/trustauthority/AuditTrustAuthorityApplication.java
+++ audit-trust-authority/src/main/java/com/frauddetection/trustauthority/AuditTrustAuthorityApplication.java
@@ -3,11 +3,9 @@ package com.frauddetection.trustauthority;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableCaching
 public class AuditTrustAuthorityApplication {
 
     public static void main(String[] args) {
